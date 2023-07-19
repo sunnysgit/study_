3446e32ab3836fd7a2298912468e5870604a5881
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2023-07-19 11:47:00 +0900

    프로그래머스 시작

---
+++ src/programmers/level0/두수의곱.java
@@ -0,0 +1,15 @@
+package programmers.level0;
+
+public class 두수의곱 {
+    public static void main(String[] args) {
+
+        int result = solution (3,4);
+        System.out.println(result);
+
+    }
+
+    static int solution (int num1, int num2) {
+
+        return num1*num2;
+    }
+}
---
+++ src/programmers/level0/두수의차.java
@@ -0,0 +1,15 @@
+package programmers.level0;
+
+public class 두수의차 {
+    public static void main(String[] args) {
+
+        int result = solution(4,5);
+        System.out.println(result);
+    }
+
+    static int solution(int num1, int num2) {
+
+        int answer = num1*num2;
+        return answer;
+    }
+}

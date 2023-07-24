6b6b5420dfc1a61dbc7ce68ad1f46eaad15984d3
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2023-07-24 21:40:28 +0900

    Update 분수의덧셈.java
    유클리드 호제법으로 최대공약수 구현

--- src/programmers/level0/분수의덧셈.java
+++ src/programmers/level0/분수의덧셈.java
@@ -9,28 +9,42 @@ public class 분수의덧셈 {
         int[] result = solution(1,2,3,4);
         System.out.println(Arrays.toString(result));
 
+
     }
 
     static int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
-
-
         answer[0] = (numer1*denom2)+(numer2*denom1);
         answer[1] = denom1*denom2;
 
-//        int min = (answer[0] > answer[1]) ? answer[1] : answer[0];
-        int min = Math.min(answer[0], answer[1]);
         int num = 0;
+//        int min = (answer[0] > answer[1]) ? answer[1] : answer[0];
+//        int min = Math.min(answer[0], answer[1]);
+//        for (int i = 1; i <= min; i++) {
+//            if (answer[0] % i == 0 && answer[1] % i == 0) {
+//                num = i;
+//            }
+//        }
 
-        for (int i = 1; i <= min; i++) {
-            if (answer[0] % i == 0 && answer[1] % i == 0) {
-                num = i;
-            }
-        }
+        num = gcd(answer[0],answer[1]);
 
         answer[0] /= num;
         answer[1] /= num;
 
+
+
         return answer;
+    }
+
+    // 유클리드 호제 - 최대공약수
+    public static int gcd(int num1, int num2) {
+
+        if (num2 == 0) {
+            return num1;
+        } else {
+            return gcd(num2, num1 % num2 );
+        }
+
+
     }
 }

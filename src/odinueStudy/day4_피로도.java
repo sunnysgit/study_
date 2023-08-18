316be29c5d66e4d2a6b58fa98f8d183ffef20e15
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2023-08-18 11:40:30 +0900

    Update day4_피로도.java

--- src/odinueStudy/day4_피로도.java
+++ src/odinueStudy/day4_피로도.java
@@ -17,16 +17,36 @@ public class day4_피로도 {
 
         System.out.print(solution(k, dungeons));
     }
+    static int answer = -1;
 
     public static int solution(int k, int[][] dungeons) {
-        int answer = -1;
-
-        // 탐험을 위해서는 k가 dungeons[0][0]번 보다 이상이여야하고, 하고나면 k-dungeons[0][1]
-        // k=80 -> 0번 하고나면 k=60
-
-        // 재귀로 돌면서 하나씩 처리할 때 마다 pass에 +1 씩 더해주고 pass가 제일 큰 경우의 수를 return
+        int[] visited = new int[dungeons.length];
+        dfs(dungeons, visited, k, 0);
 
         return answer;
+    }
+
+    private static void dfs(int[][] dungeons, int[] visited, int k, int pass) {
+        // 탐험을 위해서는 k(현재 피로도)가 dungeons[i][0](필요 피로도) 보다 이상이여야하고, 하고나면 k-dungeons[i][1] (현재 피로도 - 소모 피로도)
+        // k=80 -> 0번 하고나면 k=60
+        // 재귀로 돌면서 하나씩 처리할 때 마다 pass에 +1 씩 더해주고 pass가 제일 큰 경우의 수를 return
+
+        for (int i = 0; i < dungeons.length; i++) {
+            // 방문했던 적 없으면서 현재 피로도가 소모 피로도보다 크다면
+            if (visited[i] == 0 && k >= dungeons[i][0]) {
+                visited[i] = 1;
+                dfs(dungeons, visited, k - dungeons[i][1], pass + 1);
+                // 다음 재귀에 영향 없도록
+                visited[i] = 0;
+            }
+        }
+
+        if (answer < pass) {
+            answer = pass;
+        }
+
+
+
     }
 
 

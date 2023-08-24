52ef0d6bc34128a725a38382e93158dc8b4ce81b
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2023-08-24 18:15:50 +0900

    Create n개간격의원소들.java

---
+++ src/programmers/level0/n개간격의원소들.java
@@ -0,0 +1,19 @@
+package programmers.level0;
+
+public class n개간격의원소들 {
+    public static void main(String[] args) {
+        int[] num_list = {4, 2, 6, 1, 7, 6};
+        int n = 2;
+        System.out.println(solution(num_list, n));
+    }
+
+    public static int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+
+        for (int i = 0; i < num_list.length; i += n) {
+
+        }
+
+        return answer;
+    }
+}

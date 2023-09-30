e90325fd83aeee362d888dc9659b955f18a36a24
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2023-10-01 00:40:38 +0900

    Create 두수의연산값비교하기.java
    삼항연산자 사용

---
+++ src/programmers/level0/두수의연산값비교하기.java
@@ -0,0 +1,24 @@
+package programmers.level0;
+
+public class 두수의연산값비교하기 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 91;
+
+        System.out.println(solution(a, b));
+
+    }
+
+    static int solution(int a, int b) {
+        int answer = 0;
+
+        int dab1 = Integer.parseInt("" + a + b);
+        int dab2 = 2 * a * b;
+
+        answer = dab1 > dab2 ? dab1 : (dab1 == dab2 ? dab1 : dab2);
+
+        return answer;
+
+
+    }
+}

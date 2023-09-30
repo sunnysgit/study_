30191f63140f5657bc48b49a90ea80bc6125e4a5
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2023-10-01 00:41:08 +0900

    Update 두수의연산값비교하기.java
    Math.max() 사용

--- src/programmers/level0/두수의연산값비교하기.java
+++ src/programmers/level0/두수의연산값비교하기.java
@@ -10,15 +10,15 @@ public class 두수의연산값비교하기 {
     }
 
     static int solution(int a, int b) {
-        int answer = 0;
-
-        int dab1 = Integer.parseInt("" + a + b);
-        int dab2 = 2 * a * b;
-
-        answer = dab1 > dab2 ? dab1 : (dab1 == dab2 ? dab1 : dab2);
-
-        return answer;
-
-
+//        int answer = 0;
+//
+//        int dab1 = Integer.parseInt("" + a + b);
+//        int dab2 = 2 * a * b;
+//
+//        answer = dab1 > dab2 ? dab1 : (dab1 == dab2 ? dab1 : dab2);
+//
+//        return answer;
+//
+        return Math.max(Integer.parseInt(String.valueOf(a)+String.valueOf(b)),2*a*b);
     }
 }

218c5bbc54837e934586435de7ab4913e8f9302d
sunnysgit <112607436+sunnysgit@users.noreply.github.com>
2024-02-29 23:37:44 +0900

    0레벨 정수부분
    0레벨 부터 다시 차근차근 시작하기

---
+++ src/programmers/level0/정수부분.java
@@ -0,0 +1,9 @@
+package programmers.level0;
+
+public class 정수부분 {
+    public static void main(String[] args) {
+        double flo = 71.77;
+        int ans = (int)flo;
+        System.out.println(ans);
+    }
+}
